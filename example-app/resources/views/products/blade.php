f7bb10a90d739b0b7dc7c870435b12f4376f6930
PablusAlv <114963851+PablusAlv@users.noreply.github.com>
2023-03-07 14:54:21 -0600

    Base Procducts

---
+++ example-app/resources/views/products/blade.php
@@ -0,0 +1,14 @@
+<?php
+<title>Products View</title>
+<title>Products Editing</title>
+<title>New Product Creation</title>
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class blade extends Model
+{
+    use HasFactory;
+}
