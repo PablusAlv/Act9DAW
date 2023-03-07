622cdbc10f062e937af1f086c2cea51513c7f7a8
PablusAlv <114963851+PablusAlv@users.noreply.github.com>
2023-03-07 16:03:23 -0600

    HW5

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -2,6 +2,24 @@
 
 use Illuminate\Support\Facades\Route;
 
+$request = $_SERVER['REQUEST_URI'];
+
+switch ($request) {
+    case 'index()' :
+        require __DIR__ . '/resources/blade.php';
+        break;
+    case 'edit()' :
+        require __DIR__ . '/resources/blade.php';
+        break;
+    case 'create()' :
+        require __DIR__ . '/resources/blade.php';
+        break;
+    default:
+        http_response_code(404);
+        require __DIR__ . '/resources/blade.php';
+        break;
+}
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,6 +30,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
 
 Route::get('/', function () {
     return view('welcome');
