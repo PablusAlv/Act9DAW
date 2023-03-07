05b7ac4ae135dacb92f8a066bca11fedad2882c3
PablusAlv <114963851+PablusAlv@users.noreply.github.com>
2023-03-07 15:03:02 -0600

    Base products

---
+++ example-app/app/Http/Resources/blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Product View</title>
+</head>
+<body>
+<title>Product Editing</title>
+<title>New Product Creation</title>
+<h1>This is a heading</h1>
+<p>This is a paragraph.</p>
+
+</body>
+</html>
