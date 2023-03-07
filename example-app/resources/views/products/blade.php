2cdd72fa35ec7df0c6a51d8a17e7f80717adf430
PablusAlv <114963851+PablusAlv@users.noreply.github.com>
2023-03-07 14:55:41 -0600

    BASE PRODUCTS

--- example-app/resources/views/products/blade.php
+++ example-app/resources/views/products/blade.php
@@ -1,9 +1,10 @@
 <?php
+
+namespace App\Models;
+
 <title>Products View</title>
 <title>Products Editing</title>
 <title>New Product Creation</title>
-
-namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
